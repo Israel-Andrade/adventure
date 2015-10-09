e915467ca8e4a7ce910a99455f18753fa0938f6c
CarlosHuizarValenzuela <carloshuizar@student.hartnell.edu>
2015-10-09 11:18:57 -0700

    Create driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,62 @@
+// Name: Carlos Huizar, Israel Andrade, Thalia Villalobos, Diego Medina
+// Date: 9 Oct 2015 
+// 
+#include<iostream>
+#include<string>
+#include<cassert>
+using namespace std;
+
+char menu();
+double climbing();
+double scuba();
+double skyDive();
+double spelunk();
+double diegoDiscount();
+
+int main()
+{
+
+
+
+}
+char menu()
+{
+
+
+
+}
+double climbing()
+{
+
+
+
+
+}
+double scuba()
+{
+
+
+
+
+}
+double skyDive()
+{
+
+
+
+
+}
+double spelunk()
+{
+
+
+
+
+}
+double diegoDiscount()
+{
+
+
+
+
+}
